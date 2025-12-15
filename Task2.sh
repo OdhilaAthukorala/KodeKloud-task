06d2bcdf1424006708a0bb327b4c3f303047be8c
Odhila Athukorala <odhilajayavihan01@gmail.com>
2025-12-15 10:53:10 +0530

    task1 added

---
+++ Task2.sh
@@ -0,0 +1,9 @@
+# Tempory user setup with expiry date
+# for this first we have to log into linux server then gain root access
+# then get using sudo useradd -e YYYY-MM-DD  <username> create the user
+
+ssh steve@Am3ric@
+# give yes to login
+# enter the password of the user steve. it is given in the kodekloud infarstucture detail guide
+# now create the new user with expiry date
+sudo useradd -e 2024-04-10 yousuf
