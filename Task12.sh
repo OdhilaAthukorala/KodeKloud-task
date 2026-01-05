4788742f3e8e0f360f78c79a86c204359553576c
Odhila Athukorala <odhilajayavihan01@gmail.com>
2026-01-05 09:40:49 +0530

    edited task12

--- Task12.sh
+++ Task12.sh
@@ -30,4 +30,12 @@ systemctl enable httpd
 systemctl status httpd
 #try to access the webserver
 curl http://stapp01:8087 #or curl http://172.16.238.10:8087
+
+#this command will be executed on the host machine successfully but will fail in the jump machine
+#the reason is the iptables are not allowing the incomingtraffic on port 8087
+#so we have add port 8087 to accept tcp recquests
+#this command will be executed on the app server
+sudo iptables -I INPUT -p tcp --dport 6300 -j ACCEPT
+sudo service iptables save
+
 #it will show the default apache web page
